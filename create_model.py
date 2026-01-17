7417f3cd4d83f4036d63162069cd24eaaa512e07
Dheeraj Vishwakarma <dvashwin2004@gmail.com>
2026-01-17 20:34:55 +0530

    Update create_model.py

--- create_model.py
+++ create_model.py
@@ -3,14 +3,12 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-
 np.random.seed(42)
 df = pd.DataFrame({
     'fs': np.random.uniform(1, 3, 500),
     'reinforcement': np.random.uniform(0, 5, 500)
 })
 df['rockfall_risk'] = ((df['fs'] < 1.4) | (df['reinforcement'] < 0.4)).astype(int)
-
 
 X = df[['fs', 'reinforcement']]
 y = df['rockfall_risk']
